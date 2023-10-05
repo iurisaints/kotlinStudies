60f6bd8b09989d8dff0377a422b3cbe61a6e6475
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2023-10-05 10:44:09 -0300

    beecrowd 1008

---
+++ beecrowd/1008.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+	val reader = Scanner(System.`in`)
+	print("")
+	var a:Int = reader.nextInt()
+	print("")
+	var b:Int = reader.nextInt()
+	print("")
+	var c:Double = reader.nextDouble()
+	
+	println("NUMBER = $a")
+	println("SALARY = U$ $b*$c")
+}

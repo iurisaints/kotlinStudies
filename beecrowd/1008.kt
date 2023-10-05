524b4dcb46751aea12f0ff813d2b3036e24b78df
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2023-10-05 11:06:57 -0300

    Update 1008.kt

--- beecrowd/1008.kt
+++ beecrowd/1008.kt
@@ -8,8 +8,10 @@ fun main(args: Array<String>) {
 	print("")
 	var b:Int = reader.nextInt()
 	print("")
-	var c:Double = reader.nextDouble()
+	var c: Double = reader.nextDouble()
+	var salary: Double = b * c
+	val roundoff = String.format("%.2f", salary)
 	
 	println("NUMBER = $a")
-	println("SALARY = U$ $b*$c")
+	println("SALARY = U$ $roundoff")
 }

a37f561407d97ca9d811899b62585f4cb6bbdf33
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2023-10-09 09:26:55 -0300

    Create 1016.kt

---
+++ beecrowd/1016.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val reader = Scanner(System.`in`)
+    
+    print("")
+    var car: Int = reader.nextInt()
+    
+    var min = car*2
+    println("$min minutos")
+	
+}

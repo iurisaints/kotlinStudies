c5c7d15618ad44eae5b7b555e1de91084541f600
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2023-10-12 23:45:29 -0300

    Update 1050.kt

--- beecrowd/1050.kt
+++ beecrowd/1050.kt
@@ -7,6 +7,22 @@ fun main(args: Array<String>) {
 	
 	if (ddd == 61) {
 	    println("Brasilia")
-	}
-	//repetir para todos os outros
+	} else if (ddd == 71) {
+        println("Salvador")
+    } else if (ddd == 11) {
+        println("Sao Paulo")
+    } else if (ddd == 21){
+        println("Rio de Janeiro")
+    } else if (ddd == 32){
+        println("Juiz de Fora")
+    } else if (ddd == 19){
+        println("Campinas")
+    } else if (ddd == 27){
+        println("Vitoria")
+    } else if (ddd == 31){
+        println("Belo Horizonte")
+    } else {
+        println("DDD nao cadastrado")
+    }
+	
 }

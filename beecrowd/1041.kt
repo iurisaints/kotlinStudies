34c9cd41d7a5de317788cba91b1c7f1f14b0a5dc
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2023-10-13 11:09:45 -0300

    Update 1041.kt

--- beecrowd/1041.kt
+++ beecrowd/1041.kt
@@ -1,4 +1,23 @@
-// SE X FOR POSITIVO E Y FOR POSITIVO Q1
-//-+ Q2
-//+- Q4
-//-- Q3
+import java.util.*
+
+fun main(args: Array<String>) {
+    var reader = Scanner(System.`in`)
+    var x: Double = reader.nextDouble()
+    var y: Double = reader.nextDouble()
+    
+    if (x === 0.0 && y === 0.0){
+        println("Origem")
+    } else if (x !== 0.0 && y === 0.0 ) {
+        println("Eixo X")
+    } else if (x === 0.0 && y !== 0.0 ) {
+        println("Eixo Y")
+    } else if (x > 0.0 && y > 0.0 ) {
+        println("Q1")
+    } else if (x < 0.0 && y > 0.0 ) {
+        println("Q2")
+    } else if (x < 0.0 && y < 0.0 ) {
+        println("Q3")
+    } else if (x > 0.0 && y < 0.0 ) {
+        println("Q4")
+    }
+}

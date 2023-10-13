29c0d608e023c262929450315731bb518c8dfa62
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2023-10-12 23:40:48 -0300

    Create 1050.kt

---
+++ beecrowd/1050.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+	var reader = Scanner(System.`in`)
+	var ddd: Int = reader.nextInt()
+	
+	if (ddd == 61) {
+	    println("Brasilia")
+	}
+	//repetir para todos os outros
+}

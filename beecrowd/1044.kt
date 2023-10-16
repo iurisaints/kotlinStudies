aee1e158679eb34450a122740c3508df21ff0822
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2023-10-16 17:10:49 -0300

    Update 1044.kt

--- beecrowd/1044.kt
+++ beecrowd/1044.kt
@@ -1,11 +1,10 @@
-//NÃO ESTÁ FUNCIONAL
 import java.util.*
 
 fun main(args: Array<String>) {
     val reader = Scanner(System.`in`)
 	var a: Int = reader.nextInt()
     var b: Int = reader.nextInt()
-    if((a%b) === 0){
+    if((a%b) === 0 || (b%a) === 0){
         println("Sao Multiplos")
     } else {
         println("Nao sao Multiplos")

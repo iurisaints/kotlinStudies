fe1156904ef74e687d9d36d46f305925e31388f1
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2023-10-19 16:18:01 -0300

    Update 1046.kt

--- beecrowd/1046.kt
+++ beecrowd/1046.kt
@@ -5,8 +5,10 @@ fun main(args: Array<String>) {
     val reader = Scanner(System.`in`)
 	val ini: Int = reader.nextInt()
 	val fim: Int = reader.nextInt()
-	
-	if(ini > fim){
+  // sério, fiz o ini == fim no else if e não foi, cara... fiquei quebrando a cabeça depois e vi que devia ser antes, enfim...	
+	if (ini == fim) {
+        println("O JOGO DUROU 24 HORA(S)")
+	} else if (ini > fim) {
 	    var tempo = (24 - ini) + fim
 	    println("O JOGO DUROU $tempo HORA(S)")
 	} else {

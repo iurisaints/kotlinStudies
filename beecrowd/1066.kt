90fe9e2a583f9d8de591dfa9176d6b4a4a62bb8d
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2023-10-30 11:02:27 -0300

    Update 1066.kt

--- beecrowd/1066.kt
+++ beecrowd/1066.kt
@@ -1,4 +1,3 @@
-// EM CONSTRUÇÃO!
 import java.util.*
 
 fun main(args: Array<String>) {
@@ -8,24 +7,25 @@ fun main(args: Array<String>) {
 	var resultadoPositivo: Int = 0
 	var resultadoNegativo: Int = 0
     
-	for (i in 1..6) {
+	for (i in 1..5) {
 	    var num: Double = reader.nextDouble()
-	    if (num%2.0 === 0) {
+	    if (num%2.0 == 0.0) {
 	        resultadoPar += 1
 	    }
-	    if (num%2.0 !== 0) {
+	    if (num%2.0 != 0.0) {
 	        resultadoImpar += 1
 	    }
-        if (num >= 0) {
+        if (num > 0.0) {
            resultadoPositivo += 1
         }
-        if (num < 0) {
+        if (num < 0.0) {
 	        resultadoNegativo += 1
 	    }
 	}
-    
-    println("$resultado valores positivos")
+
+    println("$resultadoPar valor(es) par(es)")
+    println("$resultadoImpar valor(es) impar(es)")
+    println("$resultadoPositivo valor(es) positivo(s)")
+    println("$resultadoNegativo valor(es) negativo(s)")
 	
 }
-
-//em construção
